29aa4aad104de7bd834eaeb717ad358c262a71f3
bfyqjzsgkqycf <awildcrane@163.com>
2025-06-22 23:13:45 +0800

    feat(post): 添加帖子评分功能并实现相关业务逻辑
    - 在 CommentController、DiscussPostController 中添加帖子评分相关代码
    - 修改 DiscussPostMapper、DiscussPostService 以支持帖子评分更新
    - 新增 PostScoreRefreshJob 类用于定期刷新帖子分数
    - 在 QuartzConfig 中配置帖子分数刷新任务
    - 更新 RedisKeyUtil，添加帖子分数相关的 Redis key 生成方法

--- src/main/java/com/lsj/community/controller/CommentController.java
+++ src/main/java/com/lsj/community/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.lsj.community.controller;
 
+
 import com.lsj.community.entity.Comment;
 import com.lsj.community.entity.DiscussPost;
 import com.lsj.community.entity.Event;
@@ -8,7 +9,9 @@ import com.lsj.community.service.CommentService;
 import com.lsj.community.service.DiscussPostService;
 import com.lsj.community.util.CommunityConstant;
 import com.lsj.community.util.HostHolder;
+import com.lsj.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +34,9 @@ public class CommentController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
     public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
@@ -63,6 +69,9 @@ public class CommentController implements CommunityConstant {
                     .setEntityType(ENTITY_TYPE_POST)
                     .setEntityId(discussPostId);
             eventProducer.fireEvent(event);
+            // 计算帖子分数
+            String redisKey = RedisKeyUtil.getPostScoreKey();
+            redisTemplate.opsForSet().add(redisKey, discussPostId);
         }
 
         return "redirect:/discuss/detail/" + discussPostId;
