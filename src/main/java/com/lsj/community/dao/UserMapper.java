cfe0be891097cd8841ce311bf6bd8c1006b8d8a6
bfyqjzsgkqycf <awildcrane@163.com>
2025-06-22 19:18:34 +0800

    feat: 初始化用户模块
    - 新增 User 实体类
    - 创建 UserMapper 接口和 user-mapper.xml 配置文件
    - 更新 AlphaController 和 AlphaService 类- 添加数据库连接配置和 MyBatis 配置
    - 引入 MySQL 和 MyBatis 依赖

---
+++ src/main/java/com/lsj/community/dao/UserMapper.java
@@ -0,0 +1,20 @@
+package com.lsj.community.dao;
+
+@Mapper
+public interface UserMapper {
+
+    User selectById(int id);
+
+    User selectByName(String username);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+    int updateStatus(int id, int status);
+
+    int updateHeader(int id, String headerUrl);
+
+    int updatePassword(int id, String password);
+
+}
--- src/main/java/com/lsj/community/service/AlphaService.java
+++ src/main/java/com/lsj/community/service/AlphaService.java
@@ -1,11 +1,10 @@
 package com.lsj.community.service;
 
-import com.nowcoder.community.dao.AlphaDao;
+import com.lsj.community.dao.AlphaDao;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Service
 //@Scope("prototype")
